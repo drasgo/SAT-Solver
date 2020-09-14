bb5dbca3cf002b5278ceb53f358667a8bb87b67e
drasgo <tommasocastiglione@gmail.com>
2020-09-14 18:23:17 +0200

    Finished creating full structure for logic formula from DIMACS input file

---
+++ SAT/DIMACS_decoder.py
@@ -0,0 +1,137 @@
+class Formula:
+    def __init__(self, formula: str):
+        self.disjunctions = []
+        formula = self.remove_comments(formula)
+        self.num_variables, self.num_clauses, formula = self.variables_and_clauses(formula)
+        self.str_formula = self.convert_DIMACS_to_str(formula)
+        print(self.str_formula)
+        print("\n\n")
+        input()
+        self.convert_to_logic()
+
+    @staticmethod
+    def remove_comments(formula: str) -> str:
+        final = []
+        for line in formula.split("\n"):
+            if line.split(" ")[0] != "c":
+                final.append(line)
+        return "\n".join(final)
+
+    @staticmethod
+    def variables_and_clauses(formula: str):
+        variables_temp = 0
+        clauses_temp = 0
+        dim = formula.split("\n")
+        for line in dim:
+            characters = line.split(" ")
+            if characters[0] == "p" and characters[1] == "cnf" and len(characters) == 4:
+                variables_temp = characters[2]
+                clauses_temp = characters[3]
+                dim.remove(line)
+                break
+        if variables_temp != 0 and clauses_temp != 0:
+            return variables_temp, clauses_temp, "\n".join(dim)
+        else:
+            print("Error reading clauses and/or variable in DIMACS input file")
+            exit()
+
+    @staticmethod
+    def convert_DIMACS_to_str(formula: str) -> str:
+        """
+        This function converts a DIMACS set of rules in a string where the variables are divided by ORs and
+        the clauses are divided by ANDs.
+        E.g.    -111 -112 0
+                -113 -115 0
+                    ||
+                    V
+        (-111OR-112)AND(-113OR-115)
+         """
+        converted_logic = ""
+
+        for line in formula.split("\n"):
+            converted_line = ""
+            split_line = line.split(" ")
+
+            for var in split_line:
+                if var == "0":
+                    break
+                converted_line = converted_line + ("OR" if converted_line != "" else "") + var
+
+            converted_line = ("(" + converted_line + ")" if converted_line != "" else "")
+            if converted_line != "":
+                converted_logic = converted_logic + ("AND" if converted_logic != "" else "") + converted_line
+
+        return converted_logic
+
+    def convert_to_logic(self):
+        # print(len(elements))
+        # print(self.str_formula)
+        # input()
+        for disjunction in self.string_to_conjunctions(self.str_formula):
+            # print("disjunctions")
+            # print(disjunction)
+            self.disjunctions.append(Disjunction(disjunction))
+
+    @staticmethod
+    def string_to_conjunctions(elements: str) -> list:
+        return elements.split("AND")
+
+    def to_string(self) -> str:
+        formula = ""
+        for disjunction in self.disjunctions:
+            formula = formula + ("AND" if formula != "" else "") + "(" + disjunction.to_string() + ")"
+        return formula
+
+    def get_disjunctions(self) -> list:
+        return self.disjunctions
+
+    def compute_formula(self, values: list) -> bool:
+        if len(self.disjunctions) == len(values):
+            if all(disjunction.compute_disjunction(value) is True for disjunction, value in zip(self.disjunctions, values)):
+                return True
+        return False
+
+
+class Disjunction:
+    def __init__(self, elements: str):
+        self.literals = []
+        self.convert_to_logic(elements)
+
+    def convert_to_logic(self, elements: str):
+        for literal in Disjunction.string_to_disjunctions(elements.translate(str.maketrans("", "", "()"))):
+            self.literals.append(Literal(literal))
+
+    @staticmethod
+    def string_to_disjunctions(elements: str) -> list:
+        return elements.split("OR")
+
+    def to_string(self) -> str:
+        disjunction = ""
+        for literal in self.literals:
+            disjunction = disjunction + ("OR" if disjunction != "" else "") + literal.to_string()
+        return disjunction
+
+    def get_literals(self) -> list:
+        return self.literals
+
+    def compute_disjunction(self, values: list) -> bool:
+        if len(self.literals) == len(values):
+            if any(literal.get_value(value) is True for literal, value in zip(self.literals, values)):
+                return True
+        return False
+
+
+class Literal:
+    def __init__(self, literal):
+        lit = literal.split("-")
+        self.positive = True if len(lit) == 1 else False
+        self.name = lit[-1]
+
+    def to_string(self) -> str:
+        return ("" if self.positive is True else "-") + self.name
+
+    def get_value(self, boolean: bool) -> bool:
+        if self.positive:
+            return boolean
+        else:
+            return not boolean
--- SAT/main.py
+++ SAT/main.py
@@ -1,13 +1,23 @@
 import sys
 import os
+from SAT.DIMACS_decoder import Formula
+
 
 def execute_main(args):
     technique_number, input_file = grab_input_parameters(args)
     check_file_exists(input_file)
-    dimacs_input = remove_comments(read_DIMACS_input_file(input_file))
-    variables, clauses, dimacs_input = variables_and_clauses(dimacs_input)
-    logic = convert_DIMACS_to_logic(dimacs_input)
-    print(logic)
+    formula = Formula(read_DIMACS_input_file(input_file))
+    print(formula.to_string())
+    print("\n")
+    input()
+    print(len(formula.disjunctions))
+
+    print("\n")
+    input()
+    for dis in formula.disjunctions:
+        input()
+        print(len(dis.literals))
+
 
 def grab_input_parameters(args: list):
     tech = None
@@ -40,61 +50,6 @@ def read_DIMACS_input_file(file_path: str) -> str:
         dimacs = fp.read()
     return dimacs
 
-def remove_comments(dimacs: str) -> str:
-    final = []
-    for line in dimacs.split("\n"):
-        if line.split(" ")[0] != "c":
-            final.append(line)
-    return "\n".join(final)
-
-def variables_and_clauses(dimacs: str):
-    variables_temp = 0
-    clauses_temp = 0
-    dim = dimacs.split("\n")
-    for line in dim:
-        characters = line.split(" ")
-        if characters[0] == "p" and characters[1] == "cnf" and len(characters) == 4:
-            variables_temp = characters[2]
-            clauses_temp = characters[3]
-            dim.remove(line)
-            break
-    if variables_temp != 0 and clauses_temp != 0:
-        return variables_temp, clauses_temp, "\n".join(dim)
-    else:
-        print("Error reading clauses and/or variable in DIMACS input file")
-        exit()
-
-def convert_DIMACS_to_logic(dimacs: str) -> str:
-    """
-    This function converts a DIMACS set of rules in a string where the variables are divided by ORs and
-    the clauses are divided by ANDs.
-    E.g.    -111 -112 0
-            -113 -115 0
-                ||
-                V
-    (-111OR-112)AND(-113OR-115)
-     """
-    converted_logic = ""
-
-    for line in dimacs.split("\n"):
-        converted_line = ""
-        split_line = line.split(" ")
-
-        if converted_logic != "":
-            converted_logic = converted_logic + "AND"
-
-        for var in split_line:
-            if var == "0":
-                break
-
-            else:
-                if converted_line != "":
-                    converted_line = converted_line + "OR"
-                converted_line = converted_line + var
-
-        converted_logic = converted_logic + "(" + converted_line + ")"
-
-    return converted_logic
 
 if __name__ == "__main__":
     execute_main(sys.argv)
